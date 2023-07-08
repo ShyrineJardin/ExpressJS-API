4ecb44293942ea6ca6f185d4c8046c07e62279a0
ShyrineJardin <91268748+ShyrineJardin@users.noreply.github.com>
2023-07-08 20:58:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-let http = require('http')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
 
-http.createServer(function (req,res){
-	console.log(req.url)
-	console.log(req.method)
+const app = express();
+// create an app variable that stores results of the express function that initialize our express application and allow to access different methods that will make backend application easy
 
-	if(req.url === "/" && req.method === "GET"){
-		res.writeHead(200,{'Content-Type': 'text/plain'});
-		res.end('Server is running at port 4000')
-	}
-}).listen(4000);
-console.log(`Server Running on Localhost: 4000`)
+mongoose.connect('mongodb+srv://admin:Shay150715@sandbox10247.8qfjdrv.mongodb.net/an22_database_sample?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas.'));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/users', userRoutes);
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`API is now online on port ${process.env.PORT || 4000}`)
+});
